6f17d419bad04c6f2120f2804a16d1bc8c018576
Christophe Michel <christopheml@users.noreply.github.com>
2021-12-06 14:45:50 +0100

    Added ugly reflection code to run solutions automatically.

--- src/main/java/common/runners/Solutions.java
+++ src/main/java/common/runners/Solutions.java
@@ -1,23 +1,30 @@
 package common.runners;
 
-import day01.Day01;
-import day02.Day02;
-import day03.Day03;
-import day04.Day04;
-import day05.Day05;
-import day06.Day06;
-
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Solutions {
 
-    public static final List<Class<? extends Solution<?>>> ALL = List.of(
-            Day01.class,
-            Day02.class,
-            Day03.class,
-            Day04.class,
-            Day05.class,
-            Day06.class
-    );
+    public static final List<Class<? extends Solution<?>>> ALL = getSolutions();
+
+    private static List<Class<? extends Solution<?>>> getSolutions() {
+        var solutions = new ArrayList<Class< ? extends Solution<?>>>();
+        for (int day = 0; day <= 31; ++day) {
+            var solution = getSolution(day);
+            Optional.ofNullable(solution).ifPresent(solutions::add);
+        }
+        return solutions;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static Class<? extends Solution<?>> getSolution(int day) {
+        try {
+            return (Class<? extends Solution<?>>) Solutions.class.getClassLoader()
+                    .loadClass(String.format("day%1$02d.Day%1$02d", day));
+        } catch (ClassNotFoundException e) {
+            return null;
+        }
+    }
 
 }
