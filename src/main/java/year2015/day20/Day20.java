bee84f3b0e9a5895792d8e60fb61db2562ea7e46
Christophe Michel <christopheml@users.noreply.github.com>
2021-12-09 17:45:23 +0100

    Year 2015 Day 20

---
+++ src/main/java/year2015/day20/Day20.java
@@ -0,0 +1,52 @@
+package year2015.day20;
+
+import common.runners.Input;
+import common.runners.Solution;
+
+public class Day20 extends Solution<Integer> {
+
+    public Day20() {
+        super(2015, 20);
+    }
+
+    @Override
+    public Integer partOne(Input input) {
+        var target = Integer.parseInt(input.asOneLine());
+        final var houses = new int[target / 10];
+
+        for (int elf = 1; elf <= target / 10; elf++) {
+            for (int position = elf; position < houses.length; position += elf) {
+                houses[position] += elf * 10;
+            }
+        }
+
+        return findFirstHouse(target, houses);
+    }
+
+    @Override
+    public Integer partTwo(Input input) {
+        var target = Integer.parseInt(input.asOneLine());
+        final var houses = new int[target / 10];
+
+        for (int elf = 1; elf <= target / 10; elf++) {
+            for (int i = 1; i <= 50; ++i) {
+                var position = i * elf;
+                if (position < houses.length) {
+                    houses[position] += elf * 11;
+                } else break;
+            }
+        }
+
+        return findFirstHouse(target, houses);
+    }
+
+    private int findFirstHouse(int target, int[] houses) {
+        for (int i = 0; i < houses.length; i++) {
+            if (houses[i] >= target) {
+                return i;
+            }
+        }
+        throw new IllegalStateException();
+    }
+
+}
--- src/main/java/year2015/runners/Run2015.java
+++ src/main/java/year2015/runners/Run2015.java
@@ -1,11 +1,11 @@
 package year2015.runners;
 
-import year2015.day17.Day17;
+import year2015.day20.Day20;
 
 public class Run2015 {
 
     public static void main(String[] args) {
-        var solution = new Day17();
+        var solution = new Day20();
         solution.run();
     }
 
