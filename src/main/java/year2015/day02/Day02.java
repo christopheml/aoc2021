aca52a4fa63ae2290c2ffa20acdeffe1b669735b
Christophe Michel <christopheml@users.noreply.github.com>
2021-12-11 23:39:43 +0100

    Year 2015 Day 02

---
+++ src/main/java/year2015/day02/Day02.java
@@ -0,0 +1,37 @@
+package year2015.day02;
+
+import common.StringOps;
+import common.runners.Input;
+import common.runners.Solution;
+import io.vavr.collection.List;
+import io.vavr.collection.Stream;
+
+public class Day02 extends Solution<Integer> {
+
+    public Day02() {
+        super(2015, 2);
+    }
+
+    @Override
+    public Integer partOne(Input input) {
+        return presents(input)
+                .map(d -> 3 * d.get(0) * d.get(1) + 2 * d.get(1) * d.get(2) + 2 * d.get(0) * d.get(2))
+                .sum()
+                .intValue();
+    }
+
+    @Override
+    public Integer partTwo(Input input) {
+        return presents(input)
+                .map(d -> 2 * (d.get(0) + d.get(1)) + d.get(0) * d.get(1) * d.get(2))
+                .sum()
+                .intValue();
+    }
+
+    private Stream<List<Integer>> presents(Input input) {
+        return Stream.ofAll(input.asStreamOfLines())
+                .map(line -> List.ofAll(StringOps.asList(line, "x", Integer::parseInt)))
+                .map(List::sorted);
+    }
+
+}
--- src/main/java/year2015/runners/Run2015.java
+++ src/main/java/year2015/runners/Run2015.java
@@ -1,11 +1,12 @@
 package year2015.runners;
 
-import year2015.day01.Day01;
+
+import year2015.day02.Day02;
 
 public class Run2015 {
 
     public static void main(String[] args) {
-        var solution = new Day01();
+        var solution = new Day02();
         solution.run();
     }
 
