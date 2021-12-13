f9f120d6c6704b3a6f19957c6698e8ba71b96a34
Christophe Michel <christopheml@users.noreply.github.com>
2021-12-13 11:25:52 +0100

    Year 2021 Day 13: removed Fold class

--- src/main/java/year2021/day13/Day13.java
+++ src/main/java/year2021/day13/Day13.java
@@ -7,6 +7,9 @@ import io.vavr.collection.HashSet;
 import io.vavr.collection.List;
 import io.vavr.collection.Set;
 
+import java.util.Collection;
+import java.util.function.Function;
+
 import static java.lang.Integer.parseInt;
 
 public class Day13 extends Solution<Integer> {
@@ -15,55 +18,36 @@ public class Day13 extends Solution<Integer> {
         super(2021, 13);
     }
 
-    private static sealed abstract class Fold {
-        public abstract Set<Point> apply(Set<Point> points);
-
-        public static Fold parse(String text) {
-            var parts = text.split("=");
-            var position = parseInt(parts[1]);
-            return "fold along x".equals(parts[0]) ? new VerticalFold(position) : new HorizontalFold(position);
-        }
+    private Set<Point> horizontalFold(Set<Point> points, int y) {
+        return points.map(p -> {
+            if (p.y() < y) return p;
+            return new Point(p.x(), 2 * y - p.y());
+        });
     }
 
-    private final static class HorizontalFold extends Fold {
-
-        private final int y;
-
-        public HorizontalFold(int y) {
-            this.y = y;
-        }
-
-        @Override
-        public Set<Point> apply(Set<Point> points) {
-            return points.map(p -> {
-                if (p.y() < y) return p;
-                return new Point(p.x(), 2 * y - p.y());
-            });
-        }
+    private Set<Point> verticalFold(Set<Point> points, int x) {
+        return points.map(p -> {
+            if (p.x() < x) return p;
+            return new Point(2 * x - p.x(), p.y());
+        });
     }
 
-    private final static class VerticalFold extends Fold {
-
-        private final int x;
-
-        public VerticalFold(int x) {
-            this.x = x;
-        }
-
-        @Override
-        public Set<Point> apply(Set<Point> points) {
-            return points.map(p -> {
-                if (p.x() < x) return p;
-                return new Point(2 * x - p.x(), p.y());
-            });
-        }
+    public List<Function<Set<Point>, Set<Point>>> toFolds(Collection<String> lines) {
+        return List.ofAll(lines)
+                .map(line -> {
+                    var parts = line.split("=");
+                    var position = parseInt(parts[1]);
+                    return "fold along x".equals(parts[0]) ?
+                            (points) -> verticalFold(points, position) :
+                            (points) -> horizontalFold(points, position);
+                });
     }
 
     @Override
     public Integer partOne(Input input) {
         var groups = input.asGroups();
         var points = HashSet.ofAll(groups.get(0)).map(Point::fromString);
-        var folds = List.ofAll(groups.get(1)).map(Fold::parse);
+        var folds = toFolds(groups.get(1));
 
         return folds.head().apply(points).size();
     }
@@ -72,15 +56,15 @@ public class Day13 extends Solution<Integer> {
     public Integer partTwo(Input input) {
         var groups = input.asGroups();
         Set<Point> points = HashSet.ofAll(groups.get(0)).map(Point::fromString);
-        var folds = List.ofAll(groups.get(1)).map(Fold::parse);
+        var folds = toFolds(groups.get(1));
 
         for (var fold : folds) {
             points = fold.apply(points);
         }
-        var edge = points.maxBy(Point::manhattanDistance).get();
+        var edge = new Point(points.maxBy(Point::x).get().x() + 1, points.maxBy(Point::y).get().y() + 1);
         var sb = new StringBuilder();
-        for (int y = 0; y < edge.y() + 2; ++y) {
-            for (int x = 0; x < edge.x() + 2; ++x) {
+        for (int y = 0; y < edge.y(); ++y) {
+            for (int x = 0; x < edge.x(); ++x) {
                 sb.append(points.contains(new Point(x, y)) ? "#" : " ");
             }
             sb.append("\n");
